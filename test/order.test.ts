a8eff63303a71f6c85d9ac6a6fcdc5a8c83e4c79
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-17 06:43:39 -0300

    test: ensure that Order has 3 items

--- test/order.test.ts
+++ test/order.test.ts
@@ -1,7 +1,19 @@
 import Item from '../src/item'
 import Order from '../src/order'
+import OrderItem from '../src/order-item';
 
 test("Shouldn't make an order with invalid CPF", () => {
     const fakeItem = new Item("any_description", 0);
-    expect(() => new Order("invalid_cpf", [fakeItem], 0)).toThrow('Invalid CPF')
+    const fakeOrderItem = new OrderItem(fakeItem, 1);
+    expect(() => new Order("invalid_cpf", [fakeOrderItem])).toThrow('Invalid CPF')
+})
+
+test("Should make an order with 3 items", () => {
+    const orderItems = [
+        new OrderItem(new Item('any_description', 50), 1),
+        new OrderItem(new Item('any_description', 50), 1),
+        new OrderItem(new Item('any_description', 50), 1)
+    ]
+    const fakeOrder = new Order('11144477735', orderItems);
+    expect(fakeOrder.orderItems.length).toBe(3)
 })
