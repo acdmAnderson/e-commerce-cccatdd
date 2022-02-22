6dc162feb256acdb26f999bc3d6c89bec384b565
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-22 06:42:10 -0300

    feat: ensure coupon will return true when your expired date is valid

--- src/coupon.ts
+++ src/coupon.ts
@@ -1,4 +1,8 @@
 export default class Coupon {
-
+   
     constructor(readonly code: string, readonly percentage: number, readonly expiresIn: Date) { }
+
+    isExpired(currentDate: Date = new Date()) {
+        return currentDate.getTime() < this.expiresIn.getTime()
+    }
 }
