22e70e7f8b4d55c10d5f310af2be92f97b0a8f36
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-22 19:56:42 -0300

    test: ensure density will be 0 if dimensions and weight not exists

--- test/item.test.ts
+++ test/item.test.ts
@@ -7,3 +7,9 @@ test('Should calculate density', () => {
     const density = fakeItem.getDensity();
     expect(density).toBe(333)
 })
+
+test(`Shouldn’t calculate density if dimension and weight not exists`, () => {
+    const fakeItem = new Item(1, 'any_category', 'any_description', 50);
+    const density = fakeItem.getDensity();
+    expect(density).toBe(0)
+})
