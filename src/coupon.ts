fa2aae27347a2781c72458c15a7c4fd770cb0d45
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-22 06:56:52 -0300

    fix: adjuste is expired condition

--- src/coupon.ts
+++ src/coupon.ts
@@ -3,6 +3,6 @@ export default class Coupon {
     constructor(readonly code: string, readonly percentage: number, readonly expiresIn: Date) { }
 
     isExpired(currentDate: Date = new Date()) {
-        return currentDate.getTime() < this.expiresIn.getTime()
+        return currentDate.getTime() > this.expiresIn.getTime()
     }
 }
--- test/coupon.test.ts
+++ test/coupon.test.ts
@@ -1,13 +1,13 @@
 import Coupon from '../src/coupon'
 
-test('Should return true if coupon is not expired', () => {
+test('Should return false if coupon is not expired', () => {
     const fakeCoupon = new Coupon('any_code', 5, new Date('2023-02-22T23:59:59'))
     const isExpired = fakeCoupon.isExpired(new Date('2021-02-22T23:59:59'))
-    expect(isExpired).toBe(true);
+    expect(isExpired).toBe(false);
 })
 
-test('Should return false if coupon is expired', () => {
+test('Should return true if coupon is expired', () => {
     const fakeCoupon = new Coupon('any_code', 5, new Date('2020-02-22T23:59:59'))
     const isExpired = fakeCoupon.isExpired();
-    expect(isExpired).toBe(false);
+    expect(isExpired).toBe(true);
 })
