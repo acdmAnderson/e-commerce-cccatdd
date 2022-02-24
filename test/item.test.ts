930e9dec83945c7437b128066a085146728094ac
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-24 05:51:45 -0300

    test: ensure item calculate your density if it exists

--- test/item.test.ts
+++ test/item.test.ts
@@ -2,7 +2,7 @@ import Item from '../src/item';
 import Dimension from '../src/dimension'
 
 test('Should calculate density', () => {
-    const fakeDimension = new Dimension(100, 30, 10)
+    const fakeDimension = new Dimension(100, 30, 10);
     const fakeItem = new Item(1, 'any_category', 'any_description', 50, fakeDimension, 3);
     const density = fakeItem.getDensity();
     expect(density).toBe(100)
