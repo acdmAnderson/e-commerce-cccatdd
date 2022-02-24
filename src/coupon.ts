364622e318f1566189ee8e57a92041b61943f344
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-24 06:50:09 -0300

    feat: ensure coupon calculate discount

--- src/coupon.ts
+++ src/coupon.ts
@@ -1,9 +1,13 @@
 export default class Coupon {
-
+    
     constructor(readonly code: string, readonly percentage: number, readonly expiresIn?: Date) { }
-
+    
     isExpired(currentDate: Date = new Date()) {
         if (!this.expiresIn) return false;
         return currentDate.getTime() > this.expiresIn.getTime()
+    }
+
+    calculateDiscount(amount: number): number {
+        return amount * (this.percentage / 100)
     }
 }
