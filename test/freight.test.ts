5707c63f2a9956a2303b3428f8ed2e9cf200aafa
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-24 06:02:16 -0300

    test: ensure freight return min value

--- test/freight.test.ts
+++ test/freight.test.ts
@@ -18,3 +18,12 @@ test(`Shouldn’t calculate freight value if dimension and weight not exists`, (
     const value = fakeFreight.getValue();
     expect(value).toBe(0)
 })
+
+test('Should calculate min freight value', () => {
+    const fakeFreight = new Freight();
+    const fakeDimension = new Dimension(20, 15, 10)
+    const fakeItem = new Item(1, 'any_category', 'any_description', 50, fakeDimension, 0.9);
+    fakeFreight.addItem(fakeItem)
+    const value = fakeFreight.getValue();
+    expect(value).toBe(10)
+})
