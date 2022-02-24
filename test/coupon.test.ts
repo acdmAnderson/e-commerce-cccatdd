b73edc700810d46d53bb346ea08b65846ac74365
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-24 06:50:16 -0300

    test: ensure coupon calculate discount

--- test/coupon.test.ts
+++ test/coupon.test.ts
@@ -11,3 +11,9 @@ test('Should return true if coupon is expired', () => {
     const isExpired = fakeCoupon.isExpired(new Date('2023-02-22T23:59:59'));
     expect(isExpired).toBe(true);
 })
+
+test('Should return false if coupon is not expired', () => {
+    const fakeCoupon = new Coupon('any_code', 20)
+    const discount = fakeCoupon.calculateDiscount(1000);
+    expect(discount).toBe(200);
+})
