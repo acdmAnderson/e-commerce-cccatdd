a6e6eea216b1781a0a9c26788cbb6fe402d79b31
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-08 19:27:06 -0300

    test: ensure order-code create code correct

---
+++ test/unit/order-code.test.ts
@@ -0,0 +1,8 @@
+import OrderCode from '../../src/domain/entities/order-code';
+
+test('Should create a order code', () => {
+    const fakeIssueDate = new Date('2021-01-01T10:00:00')
+    const fakeSequence = 1;
+    const orderCode = new OrderCode(fakeIssueDate, fakeSequence);
+    expect(orderCode.getCode()).toBe('202100000001')
+})
