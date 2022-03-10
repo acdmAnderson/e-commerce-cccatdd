10c7086d6cb3e3ebdfcbf37a67f196e485c52195
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-10 20:46:36 -0300

    test: ensure a order can be placed without any coupon

--- test/integration/place-order.test.ts
+++ test/integration/place-order.test.ts
@@ -3,7 +3,6 @@ import PlaceOrderInput from '../../src/application/usecases/place-order/place-or
 import CouponRepository from '../../src/domain/repositories/coupon.repository'
 import OrderRepository from '../../src/domain/repositories/order.repository'
 import ItemRepository from '../../src/domain/repositories/item.repository'
-import coupon from '../../src/domain/entities/coupon';
 import Item from '../../src/domain/entities/item';
 import order from '../../src/domain/entities/order';
 import Order from '../../src/domain/entities/order';
@@ -90,4 +89,15 @@ test('Should place an order and your code', () => {
     placeOrder.execute(input);
     const output = placeOrder.execute(input);
     expect(output.code).toBe('202100000002')
+})
+
+test('Should place an order without coupon', () => {
+    const placeOrder = new PlaceOrder(makeFakeItemRepository(), makeFakeCouponRepository(), makeFakeOrderRepository());
+    const input = new PlaceOrderInput('11144477735', [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 3 },
+        { idItem: 3, quantity: 1 }
+    ])
+    const output = placeOrder.execute(input);
+    expect(output.total).toBe(250)
 })
