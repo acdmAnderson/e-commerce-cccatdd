4f6453a21e59bb4d05cc75b3880e3cb92350de14
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-10 20:22:57 -0300

    refactor: change early return condition at getDensity

--- src/domain/entities/item.ts
+++ src/domain/entities/item.ts
@@ -2,12 +2,17 @@ import Dimension from './dimension';
 
 export default class Item {
 
-    constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number, readonly dimension?: Dimension, readonly weight?: number) { }
+    constructor(
+        readonly idItem: number,
+        readonly category: string,
+        readonly description: string,
+        readonly price: number,
+        readonly dimension?: Dimension,
+        readonly weight?: number
+    ) { }
 
     getDensity(): number {
-        if (this.dimension && this.weight) {
-            return (this.weight / this.dimension.getVolume());
-        }
-        return 0;
+        if (!this.dimension || !this.weight) return 0;
+        return (this.weight / this.dimension.getVolume());
     }
 }
