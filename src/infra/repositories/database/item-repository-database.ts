bd88361b60d8755084fe600b5c41fac192c7b55d
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-22 08:27:43 -0300

    refactor: add jest events and improve item repository research

--- src/infra/repositories/database/item-repository-database.ts
+++ src/infra/repositories/database/item-repository-database.ts
@@ -8,7 +8,7 @@ export default class ItemRepositoryDatabase implements ItemRepository {
     constructor(private readonly connection: Connection) {}
 
     async getById(id: number): Promise<Item | undefined> {
-        const result = await this.connection.query(`SELECT * FROM ccca.item i where i.id_item = ${id}`, []);
+        const result = await this.connection.query('SELECT * FROM ccca.item i where i.id_item = $1', [id]);
         const [data] = result.rows;
         return new Item(data.id_item, data.category, data.description, data.price, new Dimension(data.height, data.width, data.length), data.weight);
     }
--- test/integration/item-repository-database.test.ts
+++ test/integration/item-repository-database.test.ts
@@ -1,11 +1,14 @@
 import PostgreSQLConnectionAdapter from '../../src/infra/database/postgres-connection'
 import ItemRepositoryDatabase from '../../src/infra/repositories/database/item-repository-database';
 
+const connection = new PostgreSQLConnectionAdapter();
+
+beforeEach(async () => await connection.connect())
+
 test('Should test item repository', async () => {
-    const connection = new PostgreSQLConnectionAdapter();
-    await connection.connect();
     const itemRepository = new ItemRepositoryDatabase(connection);
     const item = await itemRepository.getById(1);
     expect(item?.description).toBe('Guitarra');
-    await connection.close();
 })
+
+afterEach(async () => await connection.close())
