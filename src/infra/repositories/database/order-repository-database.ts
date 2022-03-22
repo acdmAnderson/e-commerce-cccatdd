b5b6ef143ff01485809e05547fef819e76c94a59
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-22 10:59:00 -0300

    feat: save order item

--- src/infra/repositories/database/order-repository-database.ts
+++ src/infra/repositories/database/order-repository-database.ts
@@ -12,9 +12,12 @@ export default class OrderRepositoryDatabase implements OrderRepository {
     }
 
     async save(order: Order): Promise<void> {
-        const params = [order.getCoupon(), order.getCode(), order.cpf, order.issueDate, order.getFreight().getValue(), order.sequence, order.getTotal()];
+        const params = [order.getCoupon(), order.getCode(), order.cpf.getValue(), order.issueDate, order.getFreight().getValue(), order.sequence, order.getTotal()];
         const result = await this.connection.query('INSERT INTO ccca.order (coupon, code, cpf, issue_date, freight, sequence, total) values ($1, $2, $3, $4, $5, $6, $7) returning *;', params);
         const [orderDate] = result.rows;
+        for (const { idItem, price, quantity } of order.orderItems) {
+            await this.connection.query('INSERT INTO ccca.order_item (id_order, id_item, price, quantity) values ($1, $2, $3, $4)', [orderDate.id_order, idItem, price, quantity]);
+        }
     }
 
     async count(): Promise<number> {
