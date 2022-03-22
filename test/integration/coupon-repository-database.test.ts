97089e7fd609a4064425ffc5d099bf1c71b763bf
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-22 08:23:53 -0300

    test: ensure coupon repository get a coupon by code

---
+++ test/integration/coupon-repository-database.test.ts
@@ -0,0 +1,14 @@
+import PostgreSQLConnectionAdapter from '../../src/infra/database/postgres-connection';
+import CouponRepositoryDatabase from '../../src/infra/repositories/database/coupon-repository-database';
+
+const connection = new PostgreSQLConnectionAdapter();
+
+beforeEach(async () => await connection.connect());
+
+test('Should test coupon repository', async () => {
+    const couponRepository = new CouponRepositoryDatabase(connection);
+    const coupon = await couponRepository.getByCode('VALE20');
+    expect(coupon?.isExpired(new Date('2020-01-01T10:00:00'))).toBeFalsy()
+})
+
+afterEach(async () => await connection.close())
