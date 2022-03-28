85f75a66c419e1821d6122db1c06bfb87d1185e1
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-28 11:03:13 -0300

    refactor: move all duplications to a factory method

--- test/integration/application/usecases/get-order.test.ts
+++ test/integration/application/usecases/get-order.test.ts
@@ -1,27 +1,37 @@
 import GetOrder from '../../../../src/application/usecases/get-order/get-order';
 import PlaceOrder from '../../../../src/application/usecases/place-order/place-order';
 import PlaceOrderInput from '../../../../src/application/usecases/place-order/place-order.input';
-import PostgreSQLConnectionAdapter from '../../../../src/infra/database/postgres-connection';
 import CouponRepositoryDatabase from '../../../../src/infra/repositories/database/coupon-repository-database';
 import ItemRepositoryDatabase from '../../../../src/infra/repositories/database/item-repository-database';
 import OrderRepositoryDatabase from '../../../../src/infra/repositories/database/order-repository-database';
 import PostgresConnection from '../../../util/postgres-connection';
 
 const connection = PostgresConnection.getInstance();
+const orderRepository = new OrderRepositoryDatabase(connection);
+
+beforeEach(async () => {
+    await orderRepository.clean();
+})
 
 beforeAll(async () => {
     await connection.connect();
 })
 
-test('Should return an order by code', async () => {
-    const orderRepository = new OrderRepositoryDatabase(connection);
-    await orderRepository.clean();
-    const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(connection), new CouponRepositoryDatabase(connection), orderRepository);
-    const orderInput = new PlaceOrderInput('11144477735', [
+const makeOrderInput = (): PlaceOrderInput => {
+    return new PlaceOrderInput('11144477735', [
         { idItem: 1, quantity: 1 },
         { idItem: 2, quantity: 1 },
         { idItem: 3, quantity: 3 }
     ], new Date('2021-10-10T10:00:00'), 'VALE20');
+}
+
+const makePlaceOrder = (): PlaceOrder => {
+    return new PlaceOrder(new ItemRepositoryDatabase(connection), new CouponRepositoryDatabase(connection), orderRepository);
+}
+
+test('Should return an order by code', async () => {
+    const placeOrder = makePlaceOrder();
+    const orderInput = makeOrderInput();
     await placeOrder.execute(orderInput);
     await placeOrder.execute(orderInput);
     const getOrder = new GetOrder(orderRepository);
@@ -33,14 +43,8 @@ test('Should return an order by code', async () => {
 })
 
 test('Should throw when order not exists', async () => {
-    const orderRepository = new OrderRepositoryDatabase(connection);
-    await orderRepository.clean();
-    const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(connection), new CouponRepositoryDatabase(connection), orderRepository);
-    const orderInput = new PlaceOrderInput('11144477735', [
-        { idItem: 1, quantity: 1 },
-        { idItem: 2, quantity: 1 },
-        { idItem: 3, quantity: 3 }
-    ], new Date('2021-10-10T10:00:00'), 'VALE20');
+    const placeOrder = makePlaceOrder();
+    const orderInput = makeOrderInput();
     await placeOrder.execute(orderInput);
     const getOrder = new GetOrder(orderRepository);
     const input = {
