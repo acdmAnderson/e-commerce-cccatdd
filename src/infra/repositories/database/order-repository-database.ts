afe70f69329ae39b4de35fba4e322f3192379016
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-28 11:53:20 -0300

    refactor: remove duplicated code

--- src/infra/repositories/database/order-repository-database.ts
+++ src/infra/repositories/database/order-repository-database.ts
@@ -9,30 +9,6 @@ export default class OrderRepositoryDatabase implements OrderRepository {
 
     constructor(private readonly connection: Connection) { }
 
-    async getAll(): Promise<Order[]> {
-        const orderResult = await this.connection.query('SELECT * FROM ccca.order', []);
-        const ordersData: any[] = orderResult.rows;
-        if (!ordersData.length) return [];
-        const orders = [];
-        for (const orderData of ordersData) {
-            const orderItemsResult = await this.connection.query('SELECT * FROM ccca.order_item oi WHERE oi.id_order = $1;', [orderData.id_order]);
-            const orderItemsData = orderItemsResult.rows;
-            const order = new Order(orderData.cpf, orderData.issue_date, orderData.sequence);
-            for (const { id_item, quantity } of orderItemsData) {
-                const itemResult = await this.connection.query('SELECT * FROM ccca.item i WHERE i.id_item = $1;', [id_item]);
-                const [itemData] = itemResult.rows;
-                order.addItem(new Item(itemData.id_item, itemData.category, itemData.description, itemData.price, new Dimension(itemData.height, itemData.width, itemData.length), itemData.weight), quantity);
-            }
-            if (orderData.coupon) {
-                const couponResult = await this.connection.query('SELECT * FROM ccca.coupon c WHERE c.code = $1;', [orderData.coupon]);
-                const [couponData] = couponResult.rows;
-                order.addCoupon(new Coupon(couponData.code, couponData.percentage, couponData.expire_date));
-            }
-            orders.push(order);
-        }
-        return orders;
-    }
-
     async clean(): Promise<void> {
         await this.connection.query('DELETE FROM ccca.order;', []);
         await this.connection.query('DELETE FROM ccca.order_item;', [])
@@ -57,6 +33,10 @@ export default class OrderRepositoryDatabase implements OrderRepository {
         const orderResult = await this.connection.query('SELECT * FROM ccca.order o WHERE o.code = $1;', [code]);
         const [orderData] = orderResult.rows;
         if (!orderData) return undefined;
+        return await this.getOrder(orderData);
+    }
+
+    private async getOrder(orderData: { id_order: number, cpf: string, issue_date: Date, sequence: number, coupon: string }): Promise<Order> {
         const orderItemsResult = await this.connection.query('SELECT * FROM ccca.order_item oi WHERE oi.id_order = $1;', [orderData.id_order]);
         const orderItemsData = orderItemsResult.rows;
         const order = new Order(orderData.cpf, orderData.issue_date, orderData.sequence);
@@ -71,6 +51,17 @@ export default class OrderRepositoryDatabase implements OrderRepository {
             order.addCoupon(new Coupon(couponData.code, couponData.percentage, couponData.expire_date));
         }
         return order;
+    }
+
+    async getAll(): Promise<Order[]> {
+        const orderResult = await this.connection.query('SELECT * FROM ccca.order', []);
+        const ordersData: any[] = orderResult.rows;
+        if (!ordersData.length) return [];
+        const orders = [];
+        for (const orderData of ordersData) {
+            orders.push(await this.getOrder(orderData));
+        }
+        return orders;
     }
 
 }
