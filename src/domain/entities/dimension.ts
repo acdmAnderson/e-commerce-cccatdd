e8a0545a1cf71d72497587bf63baa25224de5013
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-29 19:42:23 -0300

    feat: ensure Dimension throw if height is negative

--- src/domain/entities/dimension.ts
+++ src/domain/entities/dimension.ts
@@ -1,8 +1,10 @@
 export default class Dimension {
 
-    constructor(readonly height: number, readonly width: number, readonly depth: number) { }
+    constructor(readonly height: number, readonly width: number, readonly depth: number) {
+        if (height < 0) throw new Error('Height cannot be negative');
+    }
 
     getVolume(): number {
-        return ((this.height/100) * (this.width/100) * (this.depth/100));
+        return ((this.height / 100) * (this.width / 100) * (this.depth / 100));
     }
 }
