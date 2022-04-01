bcdbd8fee8c8a5efc6d6fe56bfbecc737ca266ff
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-31 21:42:56 -0300

    feat: ensure API return order by code

--- src/infra/api/router.ts
+++ src/infra/api/router.ts
@@ -1,4 +1,6 @@
+import GetOrderInput from '../../application/usecases/get-order/get-order.input';
 import RepositoryFactory from '../../domain/factories/repository-factory';
+import GetOrderController from '../controllers/orders/get-order.controller';
 import PlaceOrderController from '../controllers/orders/place-order.controller';
 import Http from './http';
 
@@ -10,6 +12,11 @@ export default class Router {
         this.http.route('post', '/orders', async (_params: any, body: any) => {
             const controller = new PlaceOrderController(this.repositoryFactory);
             return await controller.create(body);
+        })
+
+        this.http.route('get', '/orders/:code', async (params: any) => {
+            const controller = new GetOrderController(this.repositoryFactory);
+            return await controller.get(new GetOrderInput(params.code));
         })
     }
 }
