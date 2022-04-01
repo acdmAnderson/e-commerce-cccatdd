0fe0e2994ad2513541ff46928b163be1c0d6a924
acdmAnderson <acdm.anderson@hotmail.com>
2022-03-31 21:43:02 -0300

    test: ensure API return order by code

--- test/integration/infra/controllers/place-order-api.test.ts
+++ test/integration/infra/controllers/place-order-api.test.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import PlaceOrder from '../../../../src/application/usecases/place-order/place-order';
 import PlaceOrderInput from '../../../../src/application/usecases/place-order/place-order.input';
+import DatabaseRepositoryFactory from '../../../../src/infra/factories/database-repository-factory';
 import PostgresConnection from '../../../util/postgres-connection';
 
 const connection = PostgresConnection.getInstance();
+const repositoryFactory = new DatabaseRepositoryFactory(connection);
+const orderRepository = repositoryFactory.createOrderRepository();
+
+beforeEach(async () => {
+    await orderRepository.clean();
+})
 
 beforeAll(async () => await connection.connect());
 
@@ -14,6 +22,19 @@ test('Should create an order', async () => {
     ], new Date(), 'VALE20')
     const { data } = await axios.post('http://localhost:9001/orders', payload);
     expect(data.total).toBe(5152);
+})
+
+test('Should get an order', async () => {
+    const placeOrder = new PlaceOrder(repositoryFactory);
+    const input = new PlaceOrderInput('11144477735', [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 3 }
+    ], new Date('2021-01-01T10:00:00'));
+    await placeOrder.execute(input);
+    const { data } = await axios.get('http://localhost:9001/orders/202100000001');
+    expect(data.code).toBe('202100000001');
+    expect(data.total).toBe(6370);
 })
 
 afterAll(async () => await connection.close());
