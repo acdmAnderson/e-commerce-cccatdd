9c3aaada8b41bd882c225565fdba363e5fb50cf9
acdmAnderson <acdm.anderson@hotmail.com>
2022-04-20 20:34:53 -0300

    test: ensure StockEntry throw when operation not exists

--- test/unit/domain/entities/stock-entry.test.ts
+++ test/unit/domain/entities/stock-entry.test.ts
@@ -15,3 +15,7 @@ test('Should create a stock out', () => {
   expect(stockEntry.quantity).toBe(2)
   expect(stockEntry.date).toEqual(new Date('2020-01-01T10:00:00'))
 })
+
+test('Shoudn\'t create a StockEntry with a unexpected operation', () => {
+  expect(() => new StockEntry(1, 'wrong_operation', 6, new Date('2020-01-01T10:00:00'))).toThrow(new Error('Operation not exists'))
+})
